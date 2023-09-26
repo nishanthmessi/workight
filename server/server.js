377e5481ccfbbf3c725e313725f86c4f2dc8562b
Nishanth <nisanthrtts@gmail.com>
2023-09-26 19:50:34 +0530

    user routes injected to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js'
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
 
 // Initializing dotenv, db connection and express app
 dotenv.config()
@@ -12,10 +14,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-//API Home Route
+// API Home Route
 app.get('/', (req, res) => {
   res.send('Welcome to Workight Server')
 })
+
+// Routes
+app.use('/api/users', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on ${process.env.PORT}`)
