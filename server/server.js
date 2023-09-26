d0bd398f4b73b32cab5b3fd85de5e21a30e3b906
Nishanth <nisanthrtts@gmail.com>
2023-09-26 12:49:52 +0530

    mongodb setup done

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import connectDB from './config/db.js'
 
-// Initializing dotenv and express app
+// Initializing dotenv, db connection and express app
 dotenv.config()
-const app = express()
+connectDB()
 
+const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
