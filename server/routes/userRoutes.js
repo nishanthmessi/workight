20d07004cc5b633e9ad29122b46b40128d2ae001
Nishanth <nisanthrtts@gmail.com>
2023-09-26 20:17:47 +0530

    more user routes added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,16 @@
 import express from 'express'
-import { registerUser, authUser } from '../controllers/userController.js'
+import {
+  registerUser,
+  authUser,
+  updateUserProfile,
+  logoutUser,
+} from '../controllers/userController.js'
+import { protect } from '../middlewares/authMiddleware.js'
 
 const router = express.Router()
 router.post('/', registerUser)
 router.post('/auth', authUser)
+router.post('/logout', logoutUser)
+router.route('/profile').patch(protect, updateUserProfile)
 
 export default router
