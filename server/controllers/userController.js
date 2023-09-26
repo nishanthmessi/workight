8c646e765b346fbeee2a966fe5ada1086e9cec28
Nishanth <nisanthrtts@gmail.com>
2023-09-26 19:44:31 +0530

    user controller created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,57 @@
+import asyncHandler from 'express-async-handler'
+import User from '../models/userModel.js'
+import generateJwtToken from '../utils/generateToken.js'
 
+// @desc - Register new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    generateJwtToken(res, user._id)
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+
+// @desc - Login/Auth user
+// @route POST /api/users/auth
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    generateJwtToken(res, user._id)
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
+})
+
+export { registerUser }
